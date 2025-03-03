591819af67d2bb8bc876e58cbe3c05ee8adc6cdc
rharinath26 <rithvikharinath@gmail.com>
2025-03-03 00:06:20 -0800

    resolved merge conflicts

--- packages/frontend/src/App.jsx
+++ packages/frontend/src/App.jsx
@@ -5,33 +5,32 @@ import {
   Routes,
   Navigate,
 } from "react-router-dom";
-import styles from "./App.module.css";
 import TaskPage from "./TaskPage";
 import Login from "./Login";
 import Signup from "./Signup";
 import CalendarPage from "./CalendarPage";
 import StatsPage from "./StatsPage";
-
+import Layout from "./Layout"; 
+import ContactUsPage from "./ContactUsPage"; 
 const App = () => {
   const [token, setToken] = useState(localStorage.getItem("token") || "");
 
   return (
     <Router>
-      <div>
+      <Layout>
         <Routes>
           <Route path="/login" element={<Login setToken={setToken} />} />
           <Route path="/signup" element={<Signup setToken={setToken} />} />
           <Route
             path="/"
-            element={
-              token ? <TaskPage token={token} /> : <Navigate to="/login" />
-            }
+            element={token ? <TaskPage token={token} /> : <Navigate to="/login" />}
           />
           <Route path="/tasks" element={<TaskPage token={token} />} />{" "}
           <Route path="/calendar" element={<CalendarPage token={token} />} />{" "}
           <Route path="/stats" element={<StatsPage token={token} />} />{" "}
+          <Route path="/contact" element={<ContactUsPage />} />
         </Routes>
-      </div>
+      </Layout>
     </Router>
   );
 };
